3b3dd56dac8b94f52e13cc0ddbaf53b9c5ae0a39
gryphus-lab <80040733+gryphus-lab@users.noreply.github.com>
2025-07-29 01:23:19 +0200

    updates

--- account-service/src/main/java/com/piggymetrics/account/config/AccessTokenContextRelayConfig.java
+++ account-service/src/main/java/com/piggymetrics/account/config/AccessTokenContextRelayConfig.java
@@ -1,22 +1,15 @@
 package com.piggymetrics.account.config;
 
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.cloud.commons.security.AccessTokenContextRelay;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 
 @Configuration
 public class AccessTokenContextRelayConfig {
 
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
     @Bean
-    public AccessTokenContextRelay accessTokenContextRelay(
-            OAuth2RestTemplate oAuth2RestTemplate) {
-        return new AccessTokenContextRelay((OAuth2ClientContext) oAuth2RestTemplate);
+    public AccessTokenContextRelay accessTokenContextRelay(DefaultOAuth2ClientContext clientContext) {
+        return new AccessTokenContextRelay(clientContext);
     }
 }
