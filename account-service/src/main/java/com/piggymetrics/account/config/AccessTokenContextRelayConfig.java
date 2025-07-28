a2b32e89d35ebea94daf2bd84cf7f9eccd570721
gryphus-lab <80040733+gryphus-lab@users.noreply.github.com>
2025-07-29 01:36:02 +0200

    updates

--- account-service/src/main/java/com/piggymetrics/account/config/AccessTokenContextRelayConfig.java
+++ account-service/src/main/java/com/piggymetrics/account/config/AccessTokenContextRelayConfig.java
@@ -1,15 +1,18 @@
 package com.piggymetrics.account.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.commons.security.AccessTokenContextRelay;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.cloud.commons.security.AccessTokenContextRelay;
-import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 
 @Configuration
 public class AccessTokenContextRelayConfig {
 
+    @Autowired
+    private AccessTokenContextRelay relay;
+
     @Bean
-    public AccessTokenContextRelay accessTokenContextRelay(DefaultOAuth2ClientContext clientContext) {
-        return new AccessTokenContextRelay(clientContext);
+    public AccessTokenContextRelay accessTokenContextRelay() {
+        return relay;
     }
 }
